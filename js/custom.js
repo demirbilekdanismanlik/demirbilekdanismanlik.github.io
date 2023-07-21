5f049565f44a4180387c5c9e15b8c0f7ab9a0ca0
demirbilekdanismanlik <139779383+demirbilekdanismanlik@users.noreply.github.com>
2023-07-21 08:45:57 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+document.querySelector('.contact_us').addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("ksjldşksş");
+    
+    var phoneNumber = '+9005350165798';  // Replace with the desired phone number
+    
+    // Construct the WhatsApp URL
+    var whatsappUrl = 'https://wa.me/' + phoneNumber + "?text=Merhaba, Size ulaşmak istiyorum.";
+    
+    // Redirect the user to WhatsApp
+    window.open(whatsappUrl, '_blank');
+  });
+  
+
